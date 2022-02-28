a49096d5dc60d2b36b4163d94dfbe214b5ab892d
shihab-fw11-297 <88572629+shihab-fw11-297@users.noreply.github.com>
2022-02-28 10:04:09 +0530

    done frontend part

--- client/src/Components/admin/AdminDashbord.jsx
+++ client/src/Components/admin/AdminDashbord.jsx
@@ -1,9 +1,15 @@
-const AdminDashbord = () =>{
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from 'react';
+
+const CustomerDashbord = () =>{
+    const { auth } = useContext(AuthContext)
+    console.log(auth);
     return(
         <div className="signin-container">
-            <h1>Hello From adminDashbord</h1>
-        </div>
+          
+            <h1> <span style={{textTransform:'uppercase'}}>{auth.username}</span> Welcome To Admin Dashbord </h1>
+      </div>
     )
 }
 
-export default AdminDashbord
+export default CustomerDashbord
--- client/src/Components/client/CustomerDashbord.jsx
+++ client/src/Components/client/CustomerDashbord.jsx
@@ -1,7 +1,13 @@
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from 'react';
+
 const CustomerDashbord = () =>{
+    const { auth } = useContext(AuthContext)
+    console.log(auth);
     return(
         <div className="signin-container">
-            <h1>Welcome To Customer Dashbord</h1>
+          
+            <h1> <span style={{textTransform:'uppercase'}}>{auth.username}</span> Welcome To Customer Dashbord </h1>
       </div>
     )
 }
