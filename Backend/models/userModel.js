76f4efc1435d2565cbde7135732273ddd0d77028
Shihab Shaikh <88572629+shihab-fw11-297@users.noreply.github.com>
2022-03-01 16:11:52 +0530

    Update userModel.js

--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -11,7 +11,7 @@ const UserSchema = new mongoose.Schema({
       },
       role: {
         type: String,
-        default: "user",
+        default: "customer",
         enum: ["customer", "admin"]
       },
       username: {
